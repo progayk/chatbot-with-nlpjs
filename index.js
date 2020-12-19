567997563f7e3498bd5e95a6910d6447ddfae33a
Mayk Jony <drum.ayk@gmail.com>
2020-12-19 20:45:37 +0300

    some utterences

--- index.js
+++ index.js
@@ -2,5 +2,7 @@ const core = require('@nlpjs/core');
 const nlp = require('@nlpjs/nlp');
 // const langenmin = require('@nlpjs/lang-en-min');
 const langtr = require('@nlpjs/lang-tr');
+const requestrn = require('@nlpjs/request-rn');
 
-window.nlpjs = { ...core, ...nlp, ...langtr };
+window.nlpjs = { ...core, ...nlp, ...langtr, ...requestrn };
+
